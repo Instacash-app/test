c40e3425f16dcab9aed8bd7bb23cfb7438b57851
hmontoyag <hg.montoya97@gmail.com>
2021-03-03 17:09:43 -0500

    Decided against with game01
    game.py now finishes properly if it finds the answer before facing a wrong type in the array.  Argument is that it would realistically be much easier/faster to check for right types when the array is built (assuming another function is dedicated to that)

--- game-01/game.py
+++ game-01/game.py
@@ -1,15 +1,17 @@
 '''
-Decisions.  Should it return if the non-integer value is never reached? (e.g.: [1,4,5,"a","asd"], 6). For now, no.
+Decisions.  Should it return if the non-integer value is never reached?
+(e.g.: [1,4,5,"a","asd"], 6). For now, yes. To switch it so that it raises an
+error even if the wrong type is never reached: move if statement that raises
+ValueError in line 11 to its own for loop before.  Realistically, it might be
+easier/faster to check for typing wherever the array to be passed is being
+built.
 '''
 def sumsTwo(arr, n: int):
     values = set()
     try:
-        '''if it should return if the error value is never reached, move contents of this loop inside the other.
-        '''
         for i in arr:
             if type(i) != int:
                 raise ValueError
-        for i in arr:
             if (n-i) in values:
                 return [n-i, i]
             values.add(i)
--- game-02/app/try.ts
+++
@@ -1,4 +0,0 @@
-var i: number = 12
-var c = Math.ceil((10.1 - i)/5)
-var n = c > 0 && c < 3 ? c : 0
-console.log(n)
